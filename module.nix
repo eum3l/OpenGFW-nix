807325e3023cd6c876059eaee4ba60aac235e850
eum3l <77971322+eum3l@users.noreply.github.com>
2024-02-26 20:33:13 +0100

    nixos/opengfw: refactor settings documentation

--- module.nix
+++ module.nix
@@ -9,7 +9,7 @@ packages: {
   format = pkgs.formats.yaml {};
 
   settings =
-    if cfg.settings != {}
+    if cfg.settings != null
     then format.generate "opengfw-config.yaml" cfg.settings
     else cfg.settingsFile;
   rules =
@@ -66,25 +66,115 @@ in {
     };
 
     settings = mkOption {
-      default = {};
-      type = types.attrs;
+      default = null;
       description = mdDoc ''
         Settings passed to OpenGFW. [Example config](https://github.com/apernet/OpenGFW#example-config)
       '';
-      example = {
-        io = {
-          queueSize = 1024;
-          local = true;
+      type = types.nullOr (types.submodule {
+        options = {
+          io = mkOption {
+            description = mdDoc ''
+              IO settings.
+            '';
+            default = {};
+            type = types.submodule {
+              options = {
+                queueSize = mkOption {
+                  description = mdDoc ''
+                    IO queue size.
+                  '';
+                  type = types.int;
+                  default = 1024;
+                  example = 2048;
+                };
+                local = mkOption {
+                  description = mdDoc ''
+                    Set to false if you want to run OpenGFW on FORWARD chain.
+                  '';
+                  type = types.bool;
+                  default = true;
+                  example = false;
+                };
+              };
+            };
+          };
+          geo = mkOption {
+            description = mdDoc ''
+              The path to load specific local geoip/geosite db files.
+              If not set, they will be automatically downloaded from (Loyalsoldier/v2ray-rules-dat)[https://github.com/Loyalsoldier/v2ray-rules-dat].
+            '';
+            default = {};
+            type = types.submodule {
+              options = {
+                geoip = mkOption {
+                  description = mdDoc ''
+                    IO queue size.
+                  '';
+                  default = null;
+                  type = types.nullOr types.path;
+                };
+                geosite = mkOption {
+                  description = mdDoc ''
+                    Set to false if you want to run OpenGFW on FORWARD chain.
+                  '';
+                  default = null;
+                  type = types.nullOr types.path;
+                };
+              };
+            };
+          };
+          workers = mkOption {
+            default = {};
+            description = ''
+              Worker settings.
+            '';
+            type = types.submodule {
+              options = {
+                count = mkOption {
+                  type = types.int;
+                  description = mdDoc ''
+                    Number of workers.
+                  '';
+                  default = 4;
+                  example = 8;
+                };
+                queueSize = mkOption {
+                  type = types.int;
+                  description = mdDoc ''
+                    Worker queue size.
+                  '';
+                  default = 16;
+                  example = 32;
+                };
+                tcpMaxBufferedPagesTotal = mkOption {
+                  type = types.int;
+                  description = mdDoc ''
+                    TCP max total buffered pages.
+                  '';
+                  default = 4096;
+                  example = 8192;
+                };
+                tcpMaxBufferedPagesPerConn = mkOption {
+                  type = types.int;
+                  description = mdDoc ''
+                    TCP max total bufferd pages per connection.
+                  '';
+                  default = 64;
+                  example = 128;
+                };
+                udpMaxStreams = mkOption {
+                  type = types.int;
+                  description = mdDoc ''
+                    UDP max streams.
+                  '';
+                  default = 4096;
+                  example = 8192;
+                };
+              };
+            };
+          };
         };
-
-        workers = {
-          count = 4;
-          queueSize = 16;
-          tcpMaxBufferedPagesTotal = 4096;
-          tcpMaxBufferedPagesPerConn = 64;
-          udpMaxStreams = 4096;
-        };
-      };
+      });
     };
 
     rules = mkOption {
@@ -97,40 +187,55 @@ in {
           options = {
             name = mkOption {
               description = mdDoc "Name of the rule.";
+              example = "block google dns";
               type = types.singleLineStr;
             };
 
             action = mkOption {
-              description = mdDoc "Action of the rule. [Supported actions](https://github.com/apernet/OpenGFW?tab=readme-ov-file#supported-actions)";
+              description = mdDoc ''
+                Action of the rule. [Supported actions](https://github.com/apernet/OpenGFW?tab=readme-ov-file#supported-actions)
+              '';
               default = "allow";
+              example = "block";
               type = types.enum ["allow" "block" "drop" "modify"];
             };
 
             log = mkOption {
               description = mdDoc "Wether to enable logging for the rule.";
               default = true;
+              example = false;
               type = types.bool;
             };
 
             expr = mkOption {
-              description = mdDoc "[Expr Language](https://expr-lang.org/docs/language-definition) expression using [OpenGFW analyzers](https://github.com/apernet/OpenGFW/blob/master/docs/Analyzers.md).";
+              description = mdDoc ''
+                [Expr Language](https://expr-lang.org/docs/language-definition) expression using [OpenGFW analyzers](https://github.com/apernet/OpenGFW/blob/master/docs/Analyzers.md).
+              '';
               type = types.str;
+              example = ''dns != nil && dns.qr && any(dns.questions, {.name endsWith "google.com"})'';
             };
 
             modifier = mkOption {
               default = null;
-              description = mdDoc "Modification of specified packets when using the `modify` action. [Available modifiers](https://github.com/apernet/OpenGFW/tree/master/modifier)";
+              description = mdDoc ''
+                Modification of specified packets when using the `modify` action. [Available modifiers](https://github.com/apernet/OpenGFW/tree/master/modifier)
+              '';
               type = types.nullOr (
                 types.submodule {
                   options = {
                     name = mkOption {
                       description = mdDoc "Name of the modifier.";
                       type = types.singleLineStr;
+                      example = "dns";
                     };
 
                     args = mkOption {
                       description = mdDoc "Arguments passed to the modifier.";
                       type = types.attrs;
+                      example = {
+                        a = "0.0.0.0";
+                        aaaa = "::";
+                      };
                     };
                   };
                 }
