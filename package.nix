2b6e84bbc42b035975892691e19e8fb9f16efab5
eum3l <77971322+eum3l@users.noreply.github.com>
2024-03-06 21:47:59 +0100

    opengfw: fix check

--- package.nix
+++ package.nix
@@ -3,22 +3,25 @@
   buildGoModule,
   src,
 }:
-buildGoModule rec {
+buildGoModule {
   pname = "opengfw";
   version = "0.2.3";
 
   inherit src;
   vendorHash = "sha256-NT9KJFodTjd2HVTGDEnhfcdtl9UNaqzwTwwMHoujmAo=";
-  doCheck = false;
+
+  patches = [
+    ./v2geo-remove-test.patch
+  ];
 
   meta = with lib; {
     mainProgram = "OpenGFW";
     description = "A flexible, easy-to-use, open source implementation of GFW on Linux";
     longDescription = ''
       OpenGFW is your very own DIY Great Firewall of China, available as a flexible,
-      easy-to-use open source program on Linux. Why let the powers that be have all the fun? 
-      It's time to give power to the people and democratize censorship. 
-      Bring the thrill of cyber-sovereignty right into your home router 
+      easy-to-use open source program on Linux. Why let the powers that be have all the fun?
+      It's time to give power to the people and democratize censorship.
+      Bring the thrill of cyber-sovereignty right into your home router
       and start filtering like a pro - you too can play Big Brother.
     '';
     homepage = "https://github.com/apernet/OpenGFW";
